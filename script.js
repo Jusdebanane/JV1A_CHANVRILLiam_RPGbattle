9710ee6992f498e0a0244a72410a32a1b7c10f98
LIAM <lc.liamchanvril@gmail.com>
2022-11-23 15:32:52 +0100

    commit

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-attaquer = document.getElementById("attaque");
-boss = document.getElementById("pvBoss");
-afficheAction = document.getElementById("action");
 
-attaquer.onclick = function() {
-    pvBoss.innerHTML = pvBoss.innerHTML-10;
-    afficheAction.innerHTML = "BOSS A PERDU 10 PV !";
-}
