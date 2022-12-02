3d4dc61cd762671e826edab59dabd0cd89ace038
LIAM <lc.liamchanvril@gmail.com>
2022-12-02 15:31:35 +0100

    commit

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
 atk = document.getElementById("atk");
+def = document.getElementById("def")
+action = document.getElementById("action")
+victory = false;
+defeat = false;
+turn=1
 
 
+//stats liam
 atk_liam = 10;
-def_liam = 6;
+def_liam = 0;
 pv_liam = document.getElementById("pv_liam");
 liam = document.getElementById("liam");
 liam_ded = false;
-liam_act = true;
+liam_act = false;
 
+
+//stats clement
 atk_clement = 10;
 pv_clement = document.getElementById("pv_clement");
-clement_ded = false
+clement_ded = false;
 
-turn = 1;
-victory = false;
-defeat = false;
-already_act = false;
-   
-atk.onclick = function(){
-    if(turn == 1){
-        clement.onclick=function(){
-            if(already_act == false){
-                pv_clement.innerHTML = pv_clement.innerHTML - atk_liam;
-            turn += 1;
-            already_act = true
+
+//selection Liam
+liam.onclick = function(){
+    //verifier si le heros a deja agit
+    if(liam_act==false){
+        action.innerHTML="Qu'est ce que Liam va faire?"
+        //attaque
+        atk.onclick = function(){
+            action.innerHTML="Qui va subir l'attaque de Liam?"
+            //selection monstre
+            clement.onclick=function(){
+                //verifier si le heros a deja agit
+                if(liam_act==false){
+                    //dégats
+                    pv_clement.innerHTML = pv_clement.innerHTML - atk_liam;
+                    turn += 1;
+                    action.innerHTML = "Liam attaque Clement ";
+                    //dire que le héros a agit
+                    return liam_act = true;
+                }
+                else{
+                    action.innerHTML="Liam a déjà agit"
+                } 
             } 
-            else{
-                
-            }
         }
-    }   
-    if(turn == 2){
-        clement.onclick=function(){
-            pv_clement.innerHTML = pv_clement.innerHTML - atk_liam;
-            turn += 1;
-        }
-    }   
-    if(turn == 3){
-        clement.onclick=function(){
-            pv_clement.innerHTML = pv_clement.innerHTML - atk_liam;
-            turn += 1;
-        }
-    }   
-    if(turn == 4){
-        clement.onclick=function(){
-            pv_clement.innerHTML = pv_clement.innerHTML - atk_liam;
-            pv_liam.innerHTML = pv_liam.innerHTML - atk_clement;
-            turn = 1;
-        }
-    }   
+    }
+    else{
+        action.innerHTML="Liam a déjà agit"
+    }
 }
 
